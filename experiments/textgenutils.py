038c9e9eac941af1bd923ee91d8bb0b4e87bc53d
johnma2006 <johnma2006@gmail.com>
2023-12-01 00:02:34 -0500

    Support for KV caching in text generation

--- experiments/textgenutils.py
+++ experiments/textgenutils.py
@@ -1,6 +1,8 @@
 """Utils for generating text from a language model."""
 
 import numpy as np
+import re
+from typing import Dict
 
 import candle
 
@@ -14,7 +16,8 @@ def generate_text(model,
                   top_p: int = 0.95,
                   temperature: float = 1.0,
                   sample: bool = True,
-                  end_of_text_token: str = '<|endoftext|>'):
+                  end_of_text_token: str = '<|endoftext|>',
+                  use_kv_cache: bool = True):
     """Given a conditioning prompt, generates N more tokens using beam search.
     
     Returns a generator that yields tokens as they are generated.
@@ -29,30 +32,29 @@ def generate_text(model,
         Filter probabilities to those in the top k.
     top_p
         Nucleus sampling. Filter to top probs such that the sum prob is just less than top_p.
-
-        References:
-        [1] Ari Holtzman, Jan Buys, Li Du, Maxwell Forbes, Yejin Choi.
-            The Curious Case of Neural Text Degeneration. arXiv:1904.09751, 2019
     temperature
         Higher temperature raises the likelihood of lower probability sequences.
     sample
         True to randomly sample sequences from the distribution of probabilities, False to take argmax.
     end_of_text_token
         If provided, seeds empty generaetion with `end_of_text_token`.
+    use_kv_cache
+        If True, uses kv_cache to speed up inference.
     
     """
     if prompt == '':
         indices = candle.Tensor(np.array([tokenizer.token_to_index[end_of_text_token]]))
     else:
         indices = candle.Tensor(np.array(tokenizer.encode(prompt)))
-
+    
     generator = candle.nlp.beam_search_decoder(model, indices,
                                                n_tokens_to_generate=n_tokens_to_generate,
                                                beam_size=beam_size,
                                                top_k=top_k,
                                                top_p=top_p,
                                                temperature=temperature,
-                                               sample=sample)
+                                               sample=sample,
+                                               use_kv_cache=use_kv_cache)
 
     indices_to_decode = np.array([])
     for next_indices in generator:
@@ -63,14 +65,15 @@ def generate_text(model,
             yield token
             indices_to_decode = np.array([])
         except GeneratorExit:
+            generator.close()  # Close generator to do any required KV cache cleanup
             return
         except:
-            # Sometimes, we can't decode properly until we get a few more indices (e.g. if it falls
+            # Sometimes, tokenizer.decode fails until we get a few more indices (e.g. if it falls
             # between utf-8 byte boundaries). We accumuate those indices in `indices_to_decode` until
             # it becomes decodable
             pass
 
-        
+
 def display_stream_of_text(model,
                            tokenizer,
                            prompt: str,
@@ -80,7 +83,8 @@ def display_stream_of_text(model,
                            top_p: int = 0.95,
                            temperature: float = 1.0,
                            sample: bool = True,
-                           stop_strings: bool = True):
+                           stop_strings: Dict[str, int] = {},
+                           use_kv_cache: bool = True):
     """Given a conditioning prompt, generates N more tokens using beam search.
     
     Prints the text as it is generated.
@@ -95,39 +99,50 @@ def display_stream_of_text(model,
         Filter probabilities to those in the top k.
     top_p
         Nucleus sampling. Filter to top probs such that the sum prob is just less than top_p.
-
-        References:
-        [1] Ari Holtzman, Jan Buys, Li Du, Maxwell Forbes, Yejin Choi.
-            The Curious Case of Neural Text Degeneration. arXiv:1904.09751, 2019
     temperature
         Higher temperature raises the likelihood of lower probability sequences.
     sample
         True to randomly sample sequences from the distribution of probabilities, False to take argmax.
     stop_strings
-        If True, then stops text generation as soon as we see one of these strings.
+        Dict mapping string to how many times we can see the string before we stop generation.
+        Accepts regexes. For each string `s`, we stop generation if we see `s` at least stop_strings[s] times.
+        
+        Example:
+            stop_strings = {
+                f'John:': 1,
+                f'Taylor:': 1,
+                '\n': 1,
+                '<|endoftext|>': 1,
+                '\.|\!|\?': 4      # If we see a . or ! or ? more than 4 times total
+            }
+    use_kv_cache
+        If True, uses kv_cache to speed up inference.
     
     """
-    generator = generate_text(model, tokenizer, prompt, n_tokens_to_generate,
-                              beam_size, top_k, top_p, temperature, sample)
+    generator = generate_text(model, tokenizer, prompt, n_tokens_to_generate, beam_size,
+                              top_k, top_p, temperature, sample, use_kv_cache)
     
     response = ''
     for (tokens_generated, token) in enumerate(generator):
         # If first token, strip leading whitespace
         if tokens_generated == 0:
-            token = token.strip()
+            token = token.lstrip()
             
-        # If we see any string in `stop_strings`, end generation
+        # If we see any regex `s` at least `stop_strings[s]` times, end generation
         stop_generation = False
         for s in stop_strings:
-            if s in response + token:
-                index = (response + token).index(s)
-                token = (response + token)[len(response):index]
+            matches = list(re.finditer(s, response + token))
+            if len(matches) >= stop_strings[s]:
+                (left_index, right_index) = matches[-1].span()
+                
+                token = (response + token)[len(response):left_index]
                 stop_generation = True
 
         response += token
         print(token, end='')
         
         if stop_generation:
+            generator.close()
             break
     
     return response
